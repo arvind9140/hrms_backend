0f4469f2bde17fd945e2709c49bfa83b89b1308d
arvind9140 <ar789maurya@gmail.com>
2026-03-13 02:00:08 +0530

    first commit

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    mongodb_uri: str = "mongodb://localhost:27017"
+    mongodb_uri: str
     database_name: str = "hrms_lite"
 
     class Config:
